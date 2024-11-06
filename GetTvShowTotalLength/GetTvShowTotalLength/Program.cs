db8f8ab14917104a7dd6210e6c8d1be678d321a7
piter11251 <duszeq13@gmail.com>
2024-11-06 17:41:07 +0100

    init commit and early version of task

---
+++ GetTvShowTotalLength/GetTvShowTotalLength/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+class Program
+{
+    private static readonly HttpClient _httpClient = new HttpClient();
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            string url = "https://api.tvmaze.com/singlesearch/shows?q=the%20office&embed=episodes";
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            var episodes = JsonNode.Parse(responseBody)["_embedded"]["episodes"].AsArray();
+            int totalEpisodes = 0;
+            int totalRuntime = 0;
+            foreach(var episode in episodes)
+            {
+                totalRuntime += (int)episode["runtime"];
+            }
+            Console.WriteLine(totalRuntime);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
