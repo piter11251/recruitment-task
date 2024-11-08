0b23857cd71039c0934e39f83afa9117ef72599c
piter11251 <duszeq13@gmail.com>
2024-11-09 00:30:29 +0100

    bash script implemented

---
+++ README.md
@@ -0,0 +1,9 @@
+Działanie programów zostało przetestowane przy użyciu WSL. Po sklonowaniu repo należy nadać uprawnienia dla pliku tv-show.sh za pomocą komendy 
+```bash 
+chmod  +x tv-show.sh
+```
+I uruchomic skrypt poleceniem
+```bash
+./tv-show.sh < tv-shows.txt
+```
+
---
+++ tv-show.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+export GET_TVSHOW_TOTAL_LENGTH_BIN="GetTvShowTotalLength/GetTvShowTotalLength/bin/Debug/net8.0/GetTvShowTotalLength.exe"
+
+if [[ ! -f "$GET_TVSHOW_TOTAL_LENGTH_BIN" ]]; then 
+    echo "File not found" >&2
+    exit 1
+fi 
+
+python3 tv-time/tv-time/.venv/tv-time.py
